6ef418790feb8aa3185a101dcdd350468aa8641d
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-10-11 18:26:05 +0900

    v1.3 ParseInt, Base16

--- week07/main.go
+++ week07/main.go
@@ -17,13 +17,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		score = strings.TrimSpace(score)              //줄바꿈, 띄어쓰기 ,탭 등 제거 파이썬의 strip과 유사
-		realScore, _ := strconv.ParseFloat(score, 64) // 실수형 64비트 타입으로 형변환
+		score = strings.TrimSpace(score)                //줄바꿈, 띄어쓰기 ,탭 등 제거 파이썬의 strip과 유사
+		realScore, _ := strconv.ParseInt(score, 16, 32) // 정수형 32비트 타입으로 형변환
 
-		if realScore >= 90 {
+		if realScore >= 60 {
 			fmt.Println("A")
+			fmt.Println("%d", realScore)
 		} else {
 			fmt.Println("BCDF")
+			fmt.Println("%d", realScore)
 		}
 	}
 }

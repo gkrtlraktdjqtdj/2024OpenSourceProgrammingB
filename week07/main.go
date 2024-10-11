55094f7a34b7ccbfd0d7bda1d0671891f13fd9e6
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-10-11 17:11:50 +0900

    v0.9 Stdin

--- week07/main.go
+++ week07/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio" //Buffer input output
 	"fmt"
-	"strings"
+	"os" // Operating System
 )
 
 func main() {
-	var army string = "우리는 !가와 !민에 충성을 다하는 대한민! 육군이다."
-	armyFixed := strings.NewReplacer("!", "국")
-	fmt.Println(army)
-	fmt.Println(armyFixed.Replace(army))
+	fmt.Print("이름 입력 : ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n') //RS는 두 값를 리턴함 ,뒤에 _를 쓰면 nil값이 안보이게됨
+	fmt.Println(i)
+	fmt.Println(err) //에러가 없을 때 <nil>값을 줌
+
 }

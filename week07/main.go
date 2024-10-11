a5011bb418e7b6c5d3d722912659814e94cac048
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-10-11 18:17:16 +0900

    v1.2 Parse

--- week07/main.go
+++ week07/main.go
@@ -1,16 +1,29 @@
 package main
 
-//Buffer input output
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	// Shadowing problem
+	fmt.Print("점수 입력 : ")
+	in := bufio.NewReader(os.Stdin)
+	score, err := in.ReadString('\n')
 
-	//var float32 float32 = 9.1
-	//fmt.Println(float32)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		score = strings.TrimSpace(score)              //줄바꿈, 띄어쓰기 ,탭 등 제거 파이썬의 strip과 유사
+		realScore, _ := strconv.ParseFloat(score, 64) // 실수형 64비트 타입으로 형변환
 
-	//var fmt float32 = 9.1
-	//fmt.Println(float32)
-
-	/*var number float32
-	fmt.Println(number)*/
+		if realScore >= 90 {
+			fmt.Println("A")
+		} else {
+			fmt.Println("BCDF")
+		}
+	}
 }

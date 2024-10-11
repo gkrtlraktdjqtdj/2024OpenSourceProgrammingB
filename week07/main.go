1090b9d345e14b6d0146afc8502a2d4c1b595731
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-10-11 17:23:28 +0900

    v1.0 If

--- week07/main.go
+++ week07/main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"bufio" //Buffer input output
 	"fmt"
+	"log"
 	"os" // Operating System
 )
 
 func main() {
 	fmt.Print("이름 입력 : ")
 	r := bufio.NewReader(os.Stdin)
-	i, err := r.ReadString('\n') //RS는 두 값를 리턴함 ,뒤에 _를 쓰면 nil값이 안보이게됨
-	fmt.Println(i)
-	fmt.Println(err) //에러가 없을 때 <nil>값을 줌
+	name, err := r.ReadString('\n') //RS는 두 값를 리턴함 ,뒤에 _를 쓰면 nil값이 안보이게됨
+
+	if err != nil { //에러가 났을 경우 프로그램 종료
+		log.Fatal(err)
+	} else { //go는 c처럼 중괄호로 꼭 묶어줘야 함
+		fmt.Println(name)
+	}
 
 }

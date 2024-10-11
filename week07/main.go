9be41c254953d3652c5eb5c64f1f1f994f9d5ee5
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-10-11 17:53:31 +0900

    v1.1 Shadowing

--- week07/main.go
+++ week07/main.go
@@ -1,21 +1,16 @@
 package main
 
-import (
-	"bufio" //Buffer input output
-	"fmt"
-	"log"
-	"os" // Operating System
-)
+//Buffer input output
 
 func main() {
-	fmt.Print("이름 입력 : ")
-	r := bufio.NewReader(os.Stdin)
-	name, err := r.ReadString('\n') //RS는 두 값를 리턴함 ,뒤에 _를 쓰면 nil값이 안보이게됨
+	// Shadowing problem
 
-	if err != nil { //에러가 났을 경우 프로그램 종료
-		log.Fatal(err)
-	} else { //go는 c처럼 중괄호로 꼭 묶어줘야 함
-		fmt.Println(name)
-	}
+	//var float32 float32 = 9.1
+	//fmt.Println(float32)
 
+	//var fmt float32 = 9.1
+	//fmt.Println(float32)
+
+	/*var number float32
+	fmt.Println(number)*/
 }

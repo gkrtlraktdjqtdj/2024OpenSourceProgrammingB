4870be74fc463322af7ce21be86fc21ff6848256
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-10-25 17:01:17 +0900

    v1.7 Guess Game

--- week09/main.go
+++ week09/main.go
@@ -16,25 +16,28 @@ func main() {
 	answer := rand.Intn(6) + 1 // dice 1~6
 	fmt.Println(answer)
 
-	fmt.Print("숫자 입력 : ")
-	in := bufio.NewReader(os.Stdin)
-	input, err := in.ReadString('\n')
+	for guesses := 0; guesses < 3; guesses++ {
+		fmt.Print("숫자 입력 : ")
+		in := bufio.NewReader(os.Stdin)
+		input, err := in.ReadString('\n')
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input) // Atoi : sting > integer
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(guess)
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input) // Atoi : sting 을 integer 로 바꿈
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(guess)
 
-	if answer == guess {
-		fmt.Println("정답입니다!")
-	} else if answer > guess {
-		fmt.Println("입력하신 값은 작습니다")
-	} else {
-		fmt.Println("입력하신 값이 큽니다")
+		if answer == guess {
+			fmt.Println("정답입니다!")
+			break
+		} else if answer > guess {
+			fmt.Println("입력하신 값은 작습니다")
+		} else {
+			fmt.Println("입력하신 값이 큽니다")
+		}
 	}
 }

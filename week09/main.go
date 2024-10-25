efc7d39018a1dca1556cac4a8386aecb3667657e
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-10-25 18:11:19 +0900

    v2.0 Loop

--- week09/main.go
+++ week09/main.go
@@ -1,50 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
 	"math/rand"
-	"os"
-	"strconv"
-	"strings"
-	"time"
+	"reflect"
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
-	answer := rand.Intn(6) + 1 // dice 1~6
-	fmt.Println(answer)
+	r := fmt.Sprintf("%d\n", rand.Intn(6)+1) // Sprintf : Printf와 같은 동작, 문자열을 리턴, 출력 X
+	fmt.Println(reflect.TypeOf(r), reflect.TypeOf(rand.Intn(6)+1))
+	fmt.Printf("%T\n", r)
 
-	var win bool
-	win = false
-	for guesses := 0; guesses < 3; guesses++ { //  소괄호가 빠진다, if문도 마찬가지
-		fmt.Printf("%d번의 기회가 남았습니다 숫자 입력 : ", 3-guesses)
-		in := bufio.NewReader(os.Stdin)
-		input, err := in.ReadString('\n')
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		input = strings.TrimSpace(input)
-		guess, err := strconv.Atoi(input) // Atoi : sting 을 integer 로 바꿈
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(guess)
-
-		if answer == guess {
-			fmt.Println("정답입니다!")
-			break
-		} else if answer > guess {
-			fmt.Println("입력하신 값은 작습니다")
-		} else {
-			fmt.Println("입력하신 값이 큽니다")
-		}
-	}
-	if win {
-		fmt.Println("당신이 이겼습니다")
-	} else {
-		fmt.Printf("당신이 졌습니다 정답은 %d입니다\n", answer)
+	i := 1
+	for i <= 10 {
+		fmt.Printf("%2d점\n", i)
+		i = i + 1
 	}
 }

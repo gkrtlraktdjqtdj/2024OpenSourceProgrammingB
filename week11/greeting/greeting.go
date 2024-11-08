b9a575e8ed2773bcad33c83afc6d2d74c580f69d
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-11-08 17:33:57 +0900

    v3.5 Package

--- week11/greeting/greeting.go
+++ week11/greeting/greeting.go
@@ -2,9 +2,13 @@ package greeting
 
 import "fmt"
 
-func Hi(name string) {
-	fmt.Printf("HI %s!\n", name)
-}
-func Hello(name string) {
+func hi(name string) { // 함수 이름 첫글자는 대문자여야함
+	fmt.Printf("HI %s!\n", name) // 소문자로 시작하는 함수의 경우 접근 불가
+} //단 내부에서는 접근 가능
+func hello(name string) {
 	fmt.Printf("Hello %s!\n", name)
+}
+func EnglishGreetings(name string) { // 공개된 함수를 통해 접근
+	hello(name)
+	hi(name)
 }

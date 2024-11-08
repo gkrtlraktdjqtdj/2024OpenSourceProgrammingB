cc8fcc6df5c3981c60bd982059f2a38ba0620344
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-11-08 17:55:10 +0900

    v3.6 Prime Number

--- week11/greeting/greeting.go
+++ week11/greeting/greeting.go
@@ -1,6 +1,6 @@
 package greeting
 
-import "fmt"
+import "fmt" //package 이름은 모두 소문자고 카멜 표기법이 적용되지 않는다
 
 func hi(name string) { // 함수 이름 첫글자는 대문자여야함
 	fmt.Printf("HI %s!\n", name) // 소문자로 시작하는 함수의 경우 접근 불가
--- week11/main.go
+++ week11/main.go
@@ -1,9 +1,45 @@
 package main
 
-import "week11/greeting"
+import (
+	"fmt"
+	"log"
+	"week11/week11/keyboard"
+)
 
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 {
+		return false
+	} else {
+		i := 3
+		for i*i <= n {
+			if n%i == 0 {
+				return false
+			}
+
+			i = i + 2
+		}
+	}
+	return true
+}
 func main() {
-	// greeting.Hello("Inha")
-	// greeting.Hi("Harvard")
-	greeting.EnglishGreetings("Inha")
+	fmt.Print("첫번째(시작) 정수 입력 : ")
+	n1, err := keyboard.GetInteger()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print("두번째(끝값) 정수 입력 : ")
+	n2, err := keyboard.GetInteger()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := n1; i <= n2; i++ {
+		if isPrime(i) {
+			fmt.Printf("%d ", i)
+		}
+	}
 }

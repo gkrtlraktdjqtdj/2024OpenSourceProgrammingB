2802dd34c58e53d34e660ca124f178dc771863a4
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-11-08 18:05:36 +0900

    v3.8 Package

--- week11/keyboard/keyboard.go
+++ week11/keyboard/keyboard.go
@@ -7,7 +7,21 @@ import (
 	"strings"
 )
 
-func GetInteger() (float64, error) {
+func GetInteger() (int, error) {
+	in := bufio.NewReader(os.Stdin)
+	a, err := in.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	a = strings.TrimSpace(a)
+	number, err := strconv.Atoi(a)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
+func GetFloat() (float64, error) {
 	in := bufio.NewReader(os.Stdin)
 	a, err := in.ReadString('\n')
 	if err != nil {
--- week11/main.go
+++ week11/main.go
@@ -6,40 +6,11 @@ import (
 	"week11/week11/keyboard"
 )
 
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	} else if n == 2 {
-		return true
-	} else if n%2 == 0 {
-		return false
-	} else {
-		i := 3
-		for i*i <= n {
-			if n%i == 0 {
-				return false
-			}
-
-			i = i + 2
-		}
-	}
-	return true
-}
 func main() {
-	fmt.Print("첫번째(시작) 정수 입력 : ")
-	n1, err := keyboard.GetInteger()
+	//n, _ := keyboard.GetFloat()
+	n, err := keyboard.GetFloat()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print("두번째(끝값) 정수 입력 : ")
-	n2, err := keyboard.GetInteger()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for i := n1; i <= n2; i++ {
-		if isPrime(i) {
-			fmt.Printf("%d ", i)
-		}
-	}
+	fmt.Println(n)
 }

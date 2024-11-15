bb3cf0816ed0b5e930ea904acd134a05ef4247af
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-11-15 18:22:40 +0900

    v4.1 Array

--- week12/main.go
+++ week12/main.go
@@ -2,47 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"reflect"
 )
 
 func main() {
-	// var dates [3]time.Time
-	// dates[0] = time.Unix(0, 0)
-	// dates[2] = time.Unix(1708012346, 0)
-	// fmt.Println(dates[0], dates[1], dates[2]) // unix time, zero value, 2024-02-16 ...
+	// Create a slice by make function
+	var gpaSlice []float64
+	gpaSlice = make([]float64, 3)
+	gpaSlice[0] = 4.1
+	gpaSlice[1] = 3.9
+	gpaSlice[2] = 4.23
+	fmt.Println(gpaSlice, reflect.TypeOf(gpaSlice))
 
-	// var dates [3]time.Time = [3]time.Time{time.Unix(0, 0), time.Unix(1, 0), time.Unix(1708012346, 0)}
-	// fmt.Println(dates[0], dates[1], dates[2]) // unix time, zero value, 2024-02-16 ...
+	// Create a slice by slice litaral
+	// gpasSlice := []float64{4.1, 3.9, 4.23} // slice litaral
+	// fmt.Println(gpasSlice, reflect.TypeOf(gpasSlice))
 
-	// dates := [3]time.Time{time.Unix(0, 0), time.Unix(1, 0), time.Unix(1708012346, 0)}
-	// fmt.Println(dates[0], dates[1], dates[2]) // unix time, zero value, 2024-02-16 ...
-
-	// dates := [3]time.Time{
-	//	 time.Unix(0, 0),
-	//	 time.Unix(1, 0),
-	//	 time.Unix(1708012346, 0), // comma , 여러줄일 때 필수
-	// }
-	// fmt.Println(dates[0], dates[1], dates[2])
-
-	dates := [3]time.Time{
-		time.Unix(0, 0),
-		time.Unix(1, 0),
-		time.Unix(1708012346, 0)} // 중괄호를 내리지 않으면 가능
-	// fmt.Println(dates[0], dates[1], dates[2])
-	// fmt.Println(dates)         // array
-	// fmt.Printf("%#v\n", dates) // array litaral
-
-	// for i := 0; i <= 7; i++ {
-	// for i := 0; i < len(dates); i++ { // =을 뺴야 함
-	// 	fmt.Println(i, dates[i])
-	// }
-
-	for i, date := range dates { // index 사용
-		fmt.Println(i, date)
-	}
-
-	// for _, date := range dates { // 인덱스 미사용 like python for in, SAFE
-	// 	fmt.Println(date)
-	// }
+	// Create a slice by slicing an existing array
+	// gpas := [5]float64{3.5, 4.1, 3.9, 4.23} // array := array litaral
+	// fmt.Println(gpas, reflect.TypeOf(gpas))
+	// gpasSlice := gpas[1:4] // slice := slicing array
+	// fmt.Println(gpasSlice, reflect.TypeOf(gpasSlice))
 
 }

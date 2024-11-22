7c7499091275bcc012606b26d16217f0f94526a0
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-11-22 17:00:35 +0900

    v4.3 Slice

--- week13/main.go
+++ week13/main.go
@@ -5,6 +5,9 @@ import (
 )
 
 func main() {
+	var emptySlice []bool
+	//emptySlice = make([]bool, 5)
+	fmt.Printf("%#v\n", emptySlice) //슬라이스의 제로벨류는 nil값
 
 	gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} // array := array litaral
 	gpa_slice := gpas[1:4]                       // [3.5, 4.1, 3.9]
@@ -12,5 +15,5 @@ func main() {
 	gpas[2] = 2.71
 	//gpa_slice = append(gpa_slice, 4.3)
 	gpa_slice = append(gpa_slice, 4.3, 5.55)
-	fmt.Println(len(gpa_slice), gpa_slice, gpas) // 배열 크기의 관계가 배열 >= 슬라이스일 땐 한쪽을 바꾸면 둘 다바뀌지만 슬라이스의 크기가 배열의 크기보다 커지면 슬라이스가 따로 저장공간을 할당받는다
+	fmt.Println(len(gpa_slice), gpa_slice, gpas)
 }

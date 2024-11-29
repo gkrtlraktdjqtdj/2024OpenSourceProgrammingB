29355066addbdab7fd676f10fbfcc506356577e9
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-11-29 18:23:40 +0900

    v4.9 Struct

--- week14/main.go
+++ week14/main.go
@@ -2,22 +2,40 @@ package main
 
 import "fmt"
 
-type student struct {
-	id   int
-	name string
-	gpa  float32
+// child 5000, adult 10000, old man 7000
+type visitor struct {
+	age  int
+	cost int
+}
+
+func totalCost(visitors []visitor) int {
+	total := 0
+	for _, v := range visitors {
+		total = total + v.cost
+	}
+	return total
 }
 
 func main() {
-	var student1 student
-	student1.id = 202444051
-	student1.name = "son heungmin"
-	student1.gpa = 4.5
-	fmt.Println(student1.gpa)
+	var numVisitor int
+	fmt.Print("how many people?")
+	fmt.Scanln(&numVisitor)
 
-	var student2 student
-	student2.id = 202444052
-	student2.name = "seo jihye"
-	student2.gpa = 4.4
-	fmt.Println(student2.gpa)
+	vs := make([]visitor, numVisitor)
+	for i := 0; i < numVisitor; i++ {
+		age := 0
+		fmt.Print("how old are you?")
+		fmt.Scanln(&age)
+
+		switch { //if문으로도 만들기
+		case age < 12:
+			vs[i] = visitor{age: age, cost: 5000}
+		case age >= 12 && age < 65:
+			vs[i] = visitor{age: age, cost: 10000}
+		default:
+			vs[i] = visitor{age: age, cost: 7000}
+		}
+	}
+	fmt.Printf("Total price is %d won.", totalCost(vs))
+	//구조체는 리턴, 매개변수로 이용 가능하다
 }

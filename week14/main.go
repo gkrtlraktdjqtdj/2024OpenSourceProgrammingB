e697f291ff1995de9535cec2c3d2316fe088ba4b
thdwnsdud7107 <thdwnsdud7107@naver.com>
2024-11-29 17:49:11 +0900

    v4.8 Struct

--- week14/main.go
+++ week14/main.go
@@ -2,28 +2,22 @@ package main
 
 import "fmt"
 
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student1 student
+	student1.id = 202444051
+	student1.name = "son heungmin"
+	student1.gpa = 4.5
+	fmt.Println(student1.gpa)
 
-	student.id = 202444051
-	student.name = "son heungmin"
-	student.gpa = 4.5
-	fmt.Println(student.gpa)
-
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student2 student
 	student2.id = 202444052
 	student2.name = "seo jihye"
 	student2.gpa = 4.4
 	fmt.Println(student2.gpa)
-	// 슬라이스 ,멥은 한가지 타입만 할당받을 수 있다
-	// map[string]int map[key type]value type
-	// %#v = reflet.typrOf()
 }
